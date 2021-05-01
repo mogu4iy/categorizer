8acb0c5249ec327fc7a874a54fb7aea9ae1e68f1
Bohdan <mogu4iy.kotygoroshko@gmail.com>
2021-05-01 23:50:11 +0300

    fix speed

---
+++ coinsRegex.py
@@ -0,0 +1,21 @@
+import json
+
+COINS_FILE = r"coins.json"
+COINS_REGEX_FILE = r"coinsRegex.json"
+coins_regex = lambda coinList: '(' + '|'.join(
+    [rf"((\b({coin})(\w(\w)+)+)|((\w(\w)+)+({coin})\b))|(\b({coin})\b)" for coin in coinList]) + ')'
+
+
+def main():
+    with open(COINS_FILE, "r") as coinsListFile:
+        coinsListJSON = json.load(coinsListFile)['Cryptocurrencies']
+        with open(COINS_REGEX_FILE, "w") as coinsRegexListFile:
+            coinsRegexList = []
+            for coin in coinsListJSON:
+                coinsRegexList.append({"regex": coins_regex(coin['names']), "fullName": coin['fullName']})
+            json.dump({"Cryptocurrencies": coinsRegexList},
+                      coinsRegexListFile)
+
+
+if __name__ == '__main__':
+    main()
